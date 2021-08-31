d55e794906a692a249d96d3db6ddfee267191099
JasonCornish5 <jason.cornish5@yahoo.com>
2021-08-30 22:57:37 -0500

    first push for matt to see what i hecked up

---
+++ small-business/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello im a page!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ small-business/src/redux/state.js
@@ -0,0 +1,16 @@
+export default {
+  loggedIn: true,
+  user: {
+    username: "eree",
+    password: "",
+  },
+  listings: [
+    {
+      name: "Crux Climbing South",
+      description:
+        "Bouldering/sport climbing gym in south Austin known for its physical setting style and friendly community. After your climbing session, go grab some tacos and drinks across the street at Cosmic Coffee + Beer Garden.",
+      hours: "6AM-11PM",
+      address: "121 Pickle Rd #100, Austin, TX 78704",
+    },
+  ],
+};
